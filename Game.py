61c9aa9c307cb79ac4585818fca8a5e16c4e4d19
Aleksandar Vlasev <avlasev@gmail.com>
2015-05-31 17:38:50 -0700

    Added some methods and functions

--- Game.py
+++ Game.py
@@ -7,18 +7,26 @@ N = 20
 
 from random import randrange
 
+faces = {'unexplored': '#', 'flagged': 'F', 'mine': '*', '0': ' ', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5',
+		 '6': '6', '7': '7', '8': '8'}
+
 class Box(object):
 
 	size = X
-	covered = True
-	neighboring_mines = 0
 
-	def __init__(self, x_pos, y_pos, has_mine=False):
+	def __init__(self, x_pos, y_pos, has_mine=False, has_flag=False, pressed=False, covered=True, neighboring_mines=0):
 		self.x_pos = x_pos
 		self.y_pos = y_pos
 		self.has_mine = has_mine
+		self.has_flag = has_flag
+		self.pressed = pressed
+		self.covered = covered
+		self.neighboring_mines = neighboring_mines
 
 	def place_mine(self):
+		self.has_mine = True
+
+	def place_flag(self):
 		self.has_mine = True
 
 	def count_mines(self):
@@ -28,6 +36,14 @@ class Box(object):
 
 	def neighbors(self):
 		return []
+
+	def press(self):
+		if not self.pressed and not self.has_flag:
+			self.covered = False
+			if self.has_mine:
+				return False
+			else:
+				return True
 
 
 def generate_boxes(m=20, n=10):
@@ -42,3 +58,12 @@ def place_mines(m, n, N, field):
 		y_pos = remaining[num][1]
 		field[x_pos, y_pos].place_mine()
 		remaining.pop(num)
+
+
+def update_neighboring_mines(field):
+	for row in field:
+		for box in row:
+			box.count_mines()
+
+
+
