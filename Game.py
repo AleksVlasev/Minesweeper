d466475c9e08232a4493b7a6239208dc4925db0e
Aleksandar Vlasev <avlasev@gmail.com>
2015-06-01 22:43:41 -0700

    Added a few explanatory comments to the code.

--- Game.py
+++ Game.py
@@ -1,9 +1,17 @@
 __author__ = 'Vlasev'
 
-X = 15
-m = 15
-n = 20
-N = 30
+'''I wanted to recreate Minesweeper as an exercise in software engineering and I intended to create a full
+GUI but since I don't quite know enough about it, I decided to make a simple command line rendition. While I think
+the OOP nature of this implementation is not strictly necessary (we can do things with a nested list too),
+it was a fun way to encapsulate the meaning of different parts.
+
+We have the Box class which is basically each cell of the minefield. It contains the information necessary to play the game - 
+things like whether the cells is covered (or if we've uncovered it already), whether it has a mine or a flag, etc. Then we
+have the Field class that handles the whole minefield as one. It is from here that we actually play the game.
+
+The game itself is played from main which contains statements that direct the flow of the game - picking the dimensions
+and number of mines, selecting individual cells, planting flags, etc.
+'''
 
 from random import randrange
 from prettytable import PrettyTable
@@ -25,6 +33,8 @@ class Box(object):
 		self.has_mine = True
 
 	def get_face(self, par=0):
+		'''For the most part we need par=0 but for the LOSE screen we need par=1.
+		When par=1 we draw flags, mines and only then numbers of neighboring mines etc'''
 		if par == 0:
 			if self.has_flag:
 				return faces['flagged']
@@ -45,6 +55,7 @@ class Box(object):
 				return faces[str(self.neighboring_mines)]
 
 	def uncover(self, count):
+		'''if the cell is covered and doesn't have a flag, uncover it and if it has a mine, go boom!'''
 		if self.covered and not self.has_flag:
 			self.covered = False
 			if self.has_mine:
@@ -52,9 +63,11 @@ class Box(object):
 				return False, count
 			else:
 				return True, count - 1
+		'''if the cell has a flag we don't actually uncover the cell'''
 		elif self.has_flag:
 			print("You cannot press because there is a flag here!")
 			return True, count
+		'''similarly for uncovered cells'''
 		else:
 			# print("Oops, you have already uncovered this box!")
 			return True, count
@@ -77,7 +90,9 @@ class Field(object):
 
 	def generate_boxes(self):
 		return [[Box(i, j) for j in range(0, self.width)] for i in range(0, self.height)]
-
+	
+	'''This function makes sure that when we click for the first time at position (x, y) there 
+	are no mines there. The minefield is generated only after we've uncovered a cell.'''
 	def place_mines(self, x, y):
 		remaining = [[i, j] for j in range(0, self.width) for i in range(0, self.height)]
 		remaining.pop(remaining.index([x, y]))
@@ -90,6 +105,8 @@ class Field(object):
 			# print("Placing a mine on {}, {}".format(x_pos, y_pos))
 			remaining.pop(num)
 
+	'''this function goes through a cell's neighbors to find if they have mines. IMHO this function
+	is rather ugly and needs a rewrite.'''
 	def update_neighboring_mines(self):
 		for i in range(0, self.height):
 			for j in range(0, self.width):
@@ -100,7 +117,10 @@ class Field(object):
 							if self.field[i + t][j + s].has_mine:
 								count += 1
 				self.field[i][j].neighboring_mines = count
-
+	
+	'''This function looks at the faces of all the boxes on each turn in order to print them. It
+	can obviously be improved as to update some list of faces somewhere so we don't have to resample
+	all of them on every turn.'''
 	def get_field_faces(self, par=0):
 		field_faces = []
 		for row in self.field:
@@ -110,6 +130,7 @@ class Field(object):
 			field_faces.append(temp)
 		return field_faces
 
+	'''This function prints a nice representation of the field with prettytable'''
 	def print_field(self, par=0):
 		top_cells = ['  '] + [str(x).rjust(3, 'c') for x in range(1, self.width + 1)]
 		side_cells = [str(x).rjust(3, 'r') for x in range(1, self.height + 1)]
@@ -121,7 +142,10 @@ class Field(object):
 		for i in range(0, self.height):
 			table.add_row([side_cells[i]]+field_faces[i])
 		print(table)
-
+	
+	'''This function contains the recursive logic of uncovering a box. If the box is all good, we
+	didn't step on a mine and the box has zero neighboring mines, then we recursively uncover 
+	the neighboring boxes too until we uncover boxes with neighboring mines.'''
 	def play_box(self, i, j, count, visited):
 		try:
 			temp = count
@@ -150,7 +174,7 @@ class Field(object):
 		except TypeError:
 			print("Type Error at ({}, {}) while we have visited {} so far".format(i, j, visited))
 
-
+'''Repeatedly ask for an input among certain values until it is given.'''
 def choose_values(string, values=[]):
 	chosen = False
 	while not chosen:
@@ -159,7 +183,7 @@ def choose_values(string, values=[]):
 			chosen = True
 	return answer
 
-
+'''Repeatedly ask for an input between chosen values x and y where x < y.'''
 def choose_range(string="a number", x=0, y=1):
 	chosen = False
 	while not chosen:
